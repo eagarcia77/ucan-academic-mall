9689d8b1ee252aa0ac8efe83b060fd86f7d9ece8
eagarcia77 <131388877+eagarcia77@users.noreply.github.com>
2026-07-25 09:31:09 -0400

    Reconocer cargador Meta Quest V304 R4

--- audit_keyboard_jump_v266.js
+++ audit_keyboard_jump_v266.js
@@ -1,19 +1,21 @@
 const fs=require('fs');
 const main=fs.readFileSync('public/js/ucan_v266_keyboard_jump.js','utf8');
 const html=fs.readFileSync('public/campus.html','utf8');
+const preloader=fs.readFileSync('auth-compat-v304-r4.js','utf8');
 const legacyLoader=/ucan_v266_keyboard_jump\.js\?build=V272-20260717-XR-DESKTOP-PARITY-SPEED/.test(html);
-const questR2Loader=/ucan_v266_keyboard_jump\.js\?build=V304-20260724-QUEST-R2-LOADER/.test(html);
-const questR3Loader=/ucan_v266_keyboard_jump\.js\?build=V304-20260724-QUEST-R3-NONBLOCKING-LOADER/.test(html);
-const questLoader=questR2Loader||questR3Loader;
+const sourceV304Loader=/ucan_v266_keyboard_jump\.js\?build=V304-[^"']+/.test(html);
+const runtimeR4=/ucan_v304_quest_glass_rails_holiday_r4\.js\?build=V304-20260725-QUEST-GLASS-RAILS-HOLIDAY-R4/.test(main);
+const preloaderR4=/V304-20260725-QUEST-R4-LOADER/.test(preloader);
 const checks={
-  moduleLoaded:legacyLoader||questLoader,
-  questR3RuntimeValid:!questLoader||/ucan_v304_quest_visual_entry_r3\.js\?build=V304-20260724-QUEST-VR-NONBLOCKING-R3/.test(main),
-  oldQuestRuntimeInactive:!/ucan_v304_quest_visual_entry_r2\.js/.test(main),
+  moduleLoaded:legacyLoader||sourceV304Loader,
+  questR4RuntimeValid:runtimeR4,
+  runtimeCacheBust:preloaderR4,
+  oldQuestRuntimeInactive:!/ucan_v304_quest_visual_entry_r[23]\.js\?build=/.test(main),
   formProtection:/isTextEntryTarget/.test(main)&&/event\.stopPropagation/.test(main),
   wasdProtected:/KeyW/.test(main)&&/KeyA/.test(main)&&/KeyS/.test(main)&&/KeyD/.test(main),
   jumpEnabled:/event\.code === 'Space'/.test(main)&&/JUMP_HEIGHT/.test(main)&&/updateJump/.test(main),
   noInputPreventDefault:/if \(controlsAreBlocked\(event\)\)/.test(main)
 };
 const ok=Object.values(checks).every(Boolean);
-console.log(JSON.stringify({ok,version:'V266',checks,loader:{legacyLoader,questR2Loader,questR3Loader}},null,2));
+console.log(JSON.stringify({ok,version:'V266',checks,loader:{legacyLoader,sourceV304Loader,runtimeR4,preloaderR4}},null,2));
 if(!ok)process.exit(1);
